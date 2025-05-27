d8f563570a2223d9e64a883963293eb349c0d25d
AnonymousXDragon <rohitAsAnonymous@proton.me>
2025-05-28 01:30:41 +0530

    added services

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+require('dotenv').config()
 const app = require('./app')
 const { seedPlanets } = require('./seed.js')
 
 app.listen(3000, () => {
-//    seedPlanets();
+    if (process.env.NODE_ENV != "prod") {
+        seedPlanets();
+        console.logs(" planets are seeded successfully ! ")
+    }
+
     console.log("Server successfully running on port - " + 3000);
 })
 
